5346f1a7afc949e04a1e055aa4487ce7ca13ff87
Vladyslav <85champion24@gmail.com>
2025-03-30 19:44:21 +0200

    Image init

--- 2048/js/game.js
+++ 2048/js/game.js
@@ -3,3 +3,18 @@ const ctx = canvas.getContext("2d");
 
 const ground = new Image();
 ground.src = "img/background.png";
+
+const descr = new Image();
+descr.src = "img/descr.png";
+
+const game_over = new Image();
+game_over.src = "img/game_over.png";
+
+const new_game = new Image();
+new_game.src = "img/new_game.png";
+
+const own_record = new Image();
+own_record.src = "img/own_record.png";
+
+const score = new Image();
+score.src = "img/score.png";
